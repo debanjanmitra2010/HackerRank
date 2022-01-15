e55b052bbbda0c5444a7796f864937da1c11c414
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-15 16:03:35 -0500

    Create AMPMIntFormat.java

---
+++ AMPMIntFormat.java
@@ -0,0 +1,25 @@
+//Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+class Result {
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    String str ="";
+    if(s.charAt(8) == 'P' && Integer.parseInt(s.substring(0,2))<12){
+        int newTime = Integer.parseInt(s.substring(0,2)) + 12;
+        str = Integer.toString(newTime)+s.substring(2,8);
+    }
+    else if(s.charAt(8) == 'A' && Integer.parseInt(s.substring(0,2))==12){
+        int newTime = Integer.parseInt(s.substring(0,2)) - 12;
+        str = "0" + Integer.toString(newTime)+s.substring(2,8); 
+    }
+    
+    else{
+        str = s.substring(0,8); 
+    }
+    return str;
+}
+}
